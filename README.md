02d74cc7610bd86977fede0aff68d43b4bfa201e
jonatasbvt <jonatas.rsb@gmail.com>
2020-11-01 17:23:23 -0300

    Created main.py. Added asset sprites.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # space-invaders
+
+Recreated the project from "Tech with Tim" with some modifications, using pygame. Thanks for your awesome work!!
+
+This is simple game, based on original space invaders, but with some cool features. Download and check it!
+
+## Requirements
+
+pygame 2.0.0
+python 3.9.0
---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+import os
+import time
+import random
+
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
