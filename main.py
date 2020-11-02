99072b49e366958e55ff3337c1b36d9eb8d00a88
jonatasbvt <jonatas.rsb@gmail.com>
2020-11-02 12:11:06 -0300

    Enemies gained ability to shoot. Added player healthbar.

--- main.py
+++ main.py
@@ -110,6 +110,14 @@ class Player(Ship):
                         objs.remove(obj)
                         self.lasers.remove(laser)
 
+    def draw(self, window):
+        super().draw(window)
+        self.healthbar(window)
+
+    def healthbar(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width(), 10))
+        pygame.draw.rect(window, (0,255,0), (self.x, self.y + self.ship_img.get_height() + 10, self.ship_img.get_width() * (self.health/self.max_health), 10))
+
 class Enemy(Ship):
     COLOR_MAP = {
         "red": (RED_SPACE_SHIP, RED_LASER),
@@ -124,6 +132,12 @@ class Enemy(Ship):
 
     def move(self, vel):
         self.y += vel
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x-20, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
 
 def collide(obj1, obj2): # test if mask pixels are overlapping
     offset_x = obj2.x - obj1.x
@@ -145,7 +159,7 @@ def main():
     player_vel = 5
     player = Player(300, 650)
 
-    laser_vel = 4
+    laser_vel = 5
 
     clock = pygame.time.Clock()
 
@@ -187,7 +201,7 @@ def main():
             else:
                 continue
 
-        if len(enemies) == 0: # enemy respawn
+        if len(enemies) == 0: # enemies respawn randomly
             level += 1
             wave_length += 5
             for i in range(wave_length):
@@ -213,7 +227,14 @@ def main():
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
             enemy.move_lasers(laser_vel, player)
-            if enemy.y + enemy.get_height() > HEIGHT:
+
+            if random.randrange(0, 2*60) == 1:
+                enemy.shoot()
+
+            if collide(enemy, player):
+                player.health -= 10
+                enemies.remove(enemy)
+            elif enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
 
